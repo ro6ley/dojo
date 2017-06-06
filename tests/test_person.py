c7728c832e6f0808f57b752ae11656b71c0e1c9e
Robley Gori <robleyadrian@gmail.com>
2017-06-06 22:56:20 +0300

    [CHANGE] Update variable names

--- tests/test_person.py
+++ tests/test_person.py
@@ -10,7 +10,7 @@ class PersonTestCases(unittest.TestCase):
     Tests for the person class
     """
     def setUp(self):
-        self.new_person = Person(p_name="Person", p_type="Random")
+        self.new_person = Person(person_name="Person", person_type="Random")
         self.new_fellow = Fellow("Faith Gori")
         self.new_staff = Staff("Robley Gori")
 
@@ -19,9 +19,9 @@ class PersonTestCases(unittest.TestCase):
         Tests for the creation of a person and their attributes
         """
         self.assertTrue(self.new_person, "Person")
-        self.assertEqual(self.new_person.p_name, "Person")
-        self.assertEqual(self.new_person.p_type, "Random")
-        self.assertTrue(isinstance(self.new_person.p_id, int))
+        self.assertEqual(self.new_person.person_name, "Person")
+        self.assertEqual(self.new_person.person_type, "Random")
+        self.assertTrue(isinstance(self.new_person.person_id, int))
         self.assertTrue(isinstance(self.new_person, Person))
 
     def test_staff_object(self):
@@ -29,9 +29,9 @@ class PersonTestCases(unittest.TestCase):
         Tests for the creation of a person and their attributes
         """
         self.assertTrue(self.new_staff, "Robley Gori")
-        self.assertEqual(self.new_staff.p_name, "Robley Gori")
-        self.assertEqual(self.new_staff.p_type, "staff")
-        self.assertTrue(isinstance(self.new_staff.p_id, int))
+        self.assertEqual(self.new_staff.person_name, "Robley Gori")
+        self.assertEqual(self.new_staff.person_type, "staff")
+        self.assertTrue(isinstance(self.new_staff.person_id, int))
         self.assertTrue(isinstance(self.new_staff, Staff))
 
     def test_fellow_object(self):
@@ -39,9 +39,9 @@ class PersonTestCases(unittest.TestCase):
         Tests for the creation of a person and their attributes
         """
         self.assertTrue(self.new_fellow, "Faith Gori")
-        self.assertEqual(self.new_fellow.p_name, "Faith Gori")
-        self.assertEqual(self.new_fellow.p_type, "fellow")
-        self.assertTrue(isinstance(self.new_fellow.p_id, int))
+        self.assertEqual(self.new_fellow.person_name, "Faith Gori")
+        self.assertEqual(self.new_fellow.person_type, "fellow")
+        self.assertTrue(isinstance(self.new_fellow.person_id, int))
         self.assertTrue(isinstance(self.new_fellow, Fellow))
 
 if __name__ == "__main__":
